5bebd641705d9706fa9f9b121a2d4b9f4cba3ac3
mgttttt <92685359+mgttttt@users.noreply.github.com>
2023-09-21 11:50:26 +0500

    Update 1.3.ino

--- practika/1.3/1.3.ino
+++ practika/1.3/1.3.ino
@@ -1,27 +1,31 @@
-int currentLED = D0;  // Текущий активный светодиод
+int currentLED = D0;  
+bool buttonWasPressed = false; 
 
 void setup() {
-  pinMode(D0, OUTPUT);     // Устанавливаем пин с красным светодиодом как выход
-  pinMode(D1, OUTPUT);  // Устанавливаем пин с желтым светодиодом как выход
-  pinMode(D2, OUTPUT);   // Устанавливаем пин с зеленым светодиодом как выход
-  attachInterrupt(D3, buttonInterrupt, RISING); // Привязываем прерывание к пину D3
-  // Изначально все светодиоды выключены
+  pinMode(D0, OUTPUT);     
+  pinMode(D1, OUTPUT);  
+  pinMode(D2, OUTPUT);   
+  pinMode(D3, INPUT);      
+  
   digitalWrite(D0, LOW);
   digitalWrite(D1, LOW);
   digitalWrite(D2, LOW);
 }
 
 void loop() {
-  // В этом примере, loop() не выполняет никаких действий
-  // Вся логика управления светодиодами реализуется через аппаратное прерывание
+  int buttonState = digitalRead(D3);  
+
+
+  if (buttonState == HIGH && !buttonWasPressed) {
+    currentLED = getNextLED(currentLED);
+    updateLED(currentLED);
+    buttonWasPressed = true;
+  } else if (buttonState == LOW) {
+    buttonWasPressed = false;
+  }
 }
 
-void buttonInterrupt() {
-  currentLED = getNextLED(currentLED);
-  updateLED(currentLED);
-}
 
-// Функция для получения следующего светодиода в очереди
 int getNextLED(int current) {
   if (current == D0) {
     return D1;
@@ -32,13 +36,11 @@ int getNextLED(int current) {
   }
 }
 
-// Функция для обновления состояния светодиодов
 void updateLED(int current) {
-  // Выключаем все светодиоды
+  
   digitalWrite(D0, LOW);
   digitalWrite(D1, LOW);
   digitalWrite(D2, LOW);
 
-  // Включаем текущий светодиод
   digitalWrite(current, HIGH);
 }
