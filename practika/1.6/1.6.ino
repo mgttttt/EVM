c90c5feb1a7be5353bae8d077bfffa86fc06fac7
mgttttt <92685359+mgttttt@users.noreply.github.com>
2023-09-21 13:24:33 +0500

    Update 1.6.ino

--- practika/1.6/1.6.ino
+++ practika/1.6/1.6.ino
@@ -1,19 +1,11 @@
-const int ledPin = D0;
-const int potPin = A0; 
-const int pwmFrequency = 60; 
+int sensorValue = 0;
 
 void setup() {
-  pinMode(ledPin, PWM);
-  gpio_set_function(potPin, GPIO_FUNC_ADC);
-  adc_init();
-  adc_gpio_init(potPin);
-  adc_select_input(0);
-  adc_set_clkdiv(8); 
+  analogWriteFreq(60);
 }
 
 void loop() {
-  uint16_t potValue = adc_read();
-  uint16_t pwmValue = map(potValue, 0, 65535, 0, 1023); 
-  pwm_set_clkdiv_fractional(pwmPinToTimer(ledPin), pwmValue);
-  sleep_ms(16); 
+  sensorValue = analogRead(A0);
+  analogWrite(D0, map(sensorValue, 0, 4095, 0, 255););
+  delay(10);
 }
