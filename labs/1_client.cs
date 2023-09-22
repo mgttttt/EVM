15049a98a9ad3fcabb48e7248dcf66aa1e17b3d0
mgttttt <92685359+mgttttt@users.noreply.github.com>
2023-09-22 14:41:11 +0500

    Add files via upload

---
+++ labs/1_client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Net.Mail;
+using System.Runtime.CompilerServices;
+
+public struct Data
+{
+    public int number;
+    public int age;
+}
+
+class PipeClient
+{
+    static void Main()
+    {
+        using NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "channel", PipeDirection.InOut);
+        pipeClient.Connect();
+        Console.WriteLine("Client connected to server");
+        //StreamReader sr = new StreamReader(pipeClient);
+        byte[] bytes = new byte[Unsafe.SizeOf<Data>()];
+        pipeClient.Read(bytes, 0, bytes.Length);
+        Data received_data = Unsafe.As<byte, Data>(ref bytes[0]);
+        Console.WriteLine("Число: " + received_data.number);
+        Console.WriteLine("Возраст: " + received_data.age);
+        //Console.WriteLine($"Received data: name = {received_data.name}, age = {received_data.age}");
+        received_data.number += received_data.age; 
+        byte[] modified_bytes = new byte[Unsafe.SizeOf<Data>()];
+        Unsafe.As<byte, Data>(ref modified_bytes[0]) = received_data;
+        pipeClient.Write(modified_bytes, 0, modified_bytes.Length);
+    }
+}
---
+++ labs/1_server.cs
@@ -0,0 +1,42 @@
+﻿using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+
+public struct Data
+{
+    public int number;
+    public int age;
+}
+
+class PipeServer
+{
+    static void Main()
+    {
+        using NamedPipeServerStream pipeServer = new("channel", PipeDirection.InOut);
+        Console.WriteLine("Waiting for client connection...");
+        pipeServer.WaitForConnection();
+        Console.WriteLine("Client connected");
+        StreamWriter sw = new(pipeServer)
+        {
+            AutoFlush = true
+        };
+
+        Console.Write("Введите число: ");
+        int number_r = int.Parse(Console.ReadLine());
+        Console.Write("Введите возраст: ");
+        int age_r = int.Parse(Console.ReadLine());
+        Data msg = new()
+        {
+            number = number_r,
+            age = age_r
+        };
+
+        byte[] bytes = new byte[Unsafe.SizeOf<Data>()];
+        Unsafe.As<byte, Data>(ref bytes[0]) = msg;
+        sw.BaseStream.Write(bytes, 0, bytes.Length);
+
+        byte[] received_bytes = new byte[Unsafe.SizeOf<Data>()];
+        sw.BaseStream.Read(received_bytes, 0, received_bytes.Length);
+        Data received_data = Unsafe.As<byte, Data>(ref received_bytes[0]);
+        Console.WriteLine($"Received data: number = {received_data.number}, age = {received_data.age}");
+    }
+}
